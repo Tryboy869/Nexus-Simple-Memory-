46b383d847e1888233c6e31068903f661640e494
Tryboy869 <anzizdaouda0@gmail.com>
2025-06-26 11:39:32 +0100

    Update __init__.py

--- nsm/core/__init__.py
+++ nsm/core/__init__.py
@@ -1,13 +1,16 @@
-"""
-NSM - QR Code Memory-Based AI Memory Library
-"""
-
-__version__ = "0.1.0"
-
-from .encoder import NSMEncoder
-from .retriever import NSMRetriever
-from .chat import NSMChat
-from .interactive import chat_with_memory, quick_chat
-from .llm_client import LLMClient, create_llm_client
-
-__all__ = ["NSMEncoder", "NSMRetriever", "NSMChat", "chat_with_memory", "quick_chat", "LLMClient", "create_llm_client"]
+"""  
+NSM Core - Modules principaux  
+"""  
+  
+from .encoder import NSMEncoder  
+from .retriever import NSMRetriever  
+from .format import NSMFormat  
+from .chat import NSMChat, NSMChatManager  
+  
+__all__ = [  
+    "NSMEncoder",  
+    "NSMRetriever",   
+    "NSMFormat",  
+    "NSMChat",  
+    "NSMChatManager"  
+]
